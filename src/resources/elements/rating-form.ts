44981d335aced632fb193ad3cf641f64b7e4ec4d
sarahbarron <sarahflavin@yahoo.com>
2020-05-20 17:08:14 +0100

    added list of POIs to the Rating form

--- src/resources/elements/rating-form.ts
+++ src/resources/elements/rating-form.ts
@@ -1,13 +1,25 @@
 import { bindable } from 'aurelia-framework';
-import { Rating } from '../../services/poi-types';
+import { Rating, Poi } from '../../services/poi-types';
 
-
+/*
+* Form for submitting a rating for a Point of interest
+* */
 export class RatingForm{
 
+  // Stores a list of rating values
   @bindable
   ratings: Rating[] = []
-  selectedRating=null;
 
+  // Sores a list of POIs
+  @bindable
+  pois: Poi[] = [];
+
+  // The rating given
+  selectedRating=null;
+  // The POI selected
+  selectedPoi: Poi = null;
+
+// when a rating is given add it to the list of ratings
   makeRating(){
     const rating = {
       rating: this.selectedRating,
@@ -15,4 +27,9 @@ export class RatingForm{
     this.ratings.push(rating);
     console.log(this.ratings);
   };
+
+  constructor() {
+    this.pois.push({ name: 'Waterford', category: "Waterford Greenway", description: "Nice", image: "", longitude: 23, latitude: 100});
+    this.pois.push({ name: 'Galway', category: "Great Western Greenway", description: "Lovely", image: "", longitude: 56, latitude: 440});
+  }
 }
