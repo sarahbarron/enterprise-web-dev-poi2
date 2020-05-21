e409a73e121e2ed835f7133f23dcd4238783c236
sarahbarron <sarahflavin@yahoo.com>
2020-05-21 20:47:59 +0100

    number of pois event aggregator subscription

--- src/resources/elements/total-pois.ts
+++ src/resources/elements/total-pois.ts
@@ -1,10 +1,17 @@
 import {inject, bindable} from 'aurelia-framework';
 import {PoiService} from '../../services/poi-service';
+import {NumOfPoiUpdate} from '../../services/messages';
+import {EventAggregator} from 'aurelia-event-aggregator';
 
-@inject(PoiService)
+@inject(PoiService, EventAggregator)
 export class TotalPois{
   @bindable
   total = 0;
 
-  constructor(private ps: PoiService) {}
+  constructor(private ps: PoiService, private ea: EventAggregator) {
+    this.total = ps.total;
+    ea.subscribe(NumOfPoiUpdate, msg => {
+      this.total = msg.total;
+    });
+  }
 }
