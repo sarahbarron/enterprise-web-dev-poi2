8b9a3cbb55332224c41fa84929c96988e89e2aff
sarahbarron <sarahflavin@yahoo.com>
2020-05-31 23:05:59 +0100

    image card in place

---
+++ src/resources/elements/image-card.ts
@@ -0,0 +1,13 @@
+import { bindable, inject } from 'aurelia-framework';
+import { Poi } from '../../services/poi-types';
+import { PoiService } from '../../services/poi-service';
+
+/*
+Images
+* */
+@inject(PoiService)
+export class ImageCard {
+  @bindable singlePoi;
+
+  constructor(private ps: PoiService) {this.singlePoi = this.ps.singlePoi }
+}
